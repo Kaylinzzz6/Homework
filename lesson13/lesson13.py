03e08851d4cc3afcaee95509a7b59d18a607c264
kaylinzzz6 <kaylinzzz6@gmail.com>
2025-10-05 21:51:35 -0400

    10/5/2025

--- lesson13/lesson13.py
+++ lesson13/lesson13.py
@@ -1,1 +1,15 @@
-# Lesson 13
+# input
+day = int(input("Day:"))
+month = int(input("Month:"))
+
+if month == 2:
+    if day > 18:
+        print("The date is after February 18")
+    elif day < 18:
+        print("The date is before February 18")
+elif month > 2:
+    print("The date is after February 18")
+elif month < 2:
+    print("The date is before February 18")
+else:
+    print("Today is February 18!")

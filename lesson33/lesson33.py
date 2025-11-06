23f991804e2b457a91ffb4beb3c81c6cfdfe0cad
kaylinzzz6 <kaylinzzz6@gmail.com>
2025-11-05 23:58:55 -0500

    11/5/2025

--- lesson33/lesson33.py
+++ lesson33/lesson33.py
@@ -1,1 +1,19 @@
-# Lesson 33
+def meanymedian(a_list):
+    mean = sum(a_list) / len(a_list)
+    #sort out for median
+    new_list = []
+    while len(a_list) > 0:
+        smallest = min(a_list)
+        new_list.append(smallest)
+        a_list.remove(smallest)
+
+    if len(new_list) % 2 == 0:
+        upper = len(new_list) // 2 
+        lower = (len(new_list) // 2) - 1
+        median = (new_list[lower] + new_list[upper]) / 2
+    else:
+        median = new_list[len(new_list) // 2]
+    return mean, median
+
+testy = meanymedian([2,2,4,5,7,6])
+print(testy)

51f82b3ad8cba6265ad3df7307f8d2638f5ce408
kaylinzzz6 <kaylinzzz6@gmail.com>
2025-11-11 21:42:33 -0500

    11/11/2025

--- lesson36/lesson36.py
+++ lesson36/lesson36.py
@@ -1,1 +1,16 @@
 # Lesson 36
+def factorsfind(num):
+    factors = []
+    for x in range(1, num+1):
+        if num % x == 0:
+            factors.append(x)
+    return factors
+
+def primesfind(factors):
+    if len(factorsfind(factors)) == 2:
+        return "prime!"
+    else:
+        return "not prime!"
+
+testy = primesfind(13)
+print(testy)

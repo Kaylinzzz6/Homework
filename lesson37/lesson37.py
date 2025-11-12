0122b41750e374d5b04637605198851470af5885
kaylinzzz6 <kaylinzzz6@gmail.com>
2025-11-11 21:56:38 -0500

    11/11/2025

--- lesson37/lesson37.py
+++ lesson37/lesson37.py
@@ -1,1 +1,21 @@
 # Lesson 37
+def stringcompress(text):
+    result = ''
+    counter = 1
+
+    for i in range(1, len(text)):
+        if text[i] == text[i-1]:
+            counter += 1
+        else:
+            result += text[i-1]
+            result += str(counter)
+            counter = 1
+    result += text[-1] + str(counter)
+
+    if len(result) >= len(text):
+        return text
+    else:
+        return result
+
+tester = stringcompress("aajjkkiiiilloooooooo")
+print(tester)
